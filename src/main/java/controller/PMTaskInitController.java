f4d719bd4d13815de5cb55f0abf53615438007bc
jianwang0722 <jianw.wang@mail.utoronto.ca>
2022-11-20 13:51:27 -0500

    implemented controller class

--- src/main/java/controller/PMTaskInitController.java
+++ src/main/java/controller/PMTaskInitController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import entity.CommonProject;
+import project_manager_task_init_use_case.PMTaskInitInputBoundary;
+import project_manager_task_init_use_case.PMTaskInitRequestModel;
+import project_manager_task_init_use_case.PMTaskInitResponseModel;
+
 public class PMTaskInitController {
+
+    final PMTaskInitInputBoundary userInput;
+
+    public PMTaskInitController(PMTaskInitInputBoundary userInput) {
+        this.userInput = userInput;
+    }
+
+    PMTaskInitResponseModel create(String taskName, String taskDescription, int employeeId, CommonProject project) {
+        PMTaskInitRequestModel requestModel = new PMTaskInitRequestModel(taskName, taskDescription, employeeId, project);
+        return userInput.createTask(requestModel);
+    }
 }
