eea9b51801562fda648d5519d0664f78b271640f
KL.Wu <leonwu.wkl@gmail.com>
2022-11-20 13:41:52 -0500

    Change Department as enum.

--- src/main/java/entity/Department.java
+++ src/main/java/entity/Department.java
@@ -1,16 +1,9 @@
 package entity;
 
-import java.time.LocalDateTime;
-import java.util.Set;
-import java.util.UUID;
 
-public class Department extends CommonOrganization {
-
-    public Department(UUID oid, String name, Integer head, Set<Integer> members, String description, LocalDateTime createTime, LocalDateTime closeTime) {
-        super(oid, name, head, members, description, createTime, closeTime);
-    }
-
-    public Department(UUID oid, String name, Integer head, Set<Integer> members, String description, LocalDateTime createTime) {
-        super(oid, name, head, members, description, createTime);
-    }
+public enum Department {
+    DEPARTMENT_1,
+    DEPARTMENT_2,
+    DEPARTMENT_3,
+    DEPARTMENT_4
 }
--- src/main/java/salary_calculator/SalaryPositionMap.java
+++ src/main/java/salary_calculator/SalaryPositionMap.java
@@ -6,11 +6,11 @@ public class SalaryPositionMap {
     static Integer getHourSalaryByPosition(Position position) throws IllegalArgumentException {
         if (position == Position.CEO) {
             return 100;
-        } else if (position == Position.DepartmentHead) {
+        } else if (position == Position.DEPARTMENT_HEAD) {
             return 50;
-        } else if (position == Position.ProjectHead) {
+        } else if (position == Position.PROJECT_HEAD) {
             return 25;
-        } else if (position == Position.Member) {
+        } else if (position == Position.MEMBER) {
             return 20;
         } else throw new IllegalArgumentException();
     }
