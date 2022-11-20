2a436162c173daa6f25f270b961e54b73b33fa9d
kaizen-w <113033661+kaizen-w@users.noreply.github.com>
2022-11-19 21:13:36 -0500

    Initial commit for RankEmployeesUseCase. Missing implementation for Presenter and Data Access.

---
+++ src/main/java/RankEmployeesUseCase/RankPresenter.java
@@ -0,0 +1,9 @@
+package RankEmployeesUseCase;
+import RankEmployeesUseCase.RankOutputBoundary;
+public class RankPresenter implements RankOutputBoundary {
+
+    public RankResponseModel displayRanking(RankResponseModel employeeRanking){
+        return null;
+    }
+
+}
