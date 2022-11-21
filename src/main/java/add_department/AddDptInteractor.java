b6ce72c3abc9bd170f185207532143fa2a79b1dd
Zeyu Zhang <zeyuzh_zhang@hotmail.com>
2022-11-21 00:18:50 -0500

    add department feature

--- src/main/java/add_department/AddDptInteractor.java
+++ src/main/java/add_department/AddDptInteractor.java
@@ -3,13 +3,21 @@ package add_department;
 import entity.DepartmentFactory;
 
 public class AddDptInteractor implements AddDptInputBoundary{
-    final AddDptInputBoundary inputBoundary;
+    final AddDptDsGateway dsGateway;
     final AddDptPresenter presenter;
     final DepartmentFactory factory;
 
-    public AddDptInteractor(AddDptInputBoundary inputBoundary, AddDptPresenter presenter, DepartmentFactory factory) {
-        this.inputBoundary = inputBoundary;
+    public AddDptInteractor(AddDptDsGateway dsGateway, AddDptPresenter presenter, DepartmentFactory factory) {
+        this.dsGateway = dsGateway;
         this.presenter = presenter;
         this.factory = factory;
+    }
+
+    @Override
+    public AddDptResponseModel create(AddDptRequestModel requestModel) {
+        if (dsGateway.existByName(requestModel.getName())){
+            presenter.prepareFailView("Name already exists.");
+
+        }
     }
 }
