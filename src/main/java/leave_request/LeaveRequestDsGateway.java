d61f87897e9b38c4d11f39bc20f09c474508c7d0
tlan4 <tim.lan100102@gmail.com>
2022-11-21 14:35:28 -0500

    Modified Interactor and related classes to use RoleAllowed.

--- src/main/java/leave_request/LeaveRequestDsGateway.java
+++ src/main/java/leave_request/LeaveRequestDsGateway.java
@@ -1,5 +1,10 @@
 package leave_request;
 
+import entity.Role;
+
+import java.util.List;
+
 public interface LeaveRequestDsGateway {
     void save(LeaveRequestDsRequestModel requestModel);
+    List<Role> getRoles(Integer uid);
 }
--- src/main/java/leave_request/LeaveRequestInteractor.java
+++ src/main/java/leave_request/LeaveRequestInteractor.java
@@ -2,6 +2,9 @@ package leave_request;
 
 import entity.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LeaveRequestInteractor implements LeaveRequestInputBoundary {
     private final LeaveRequestDsGateway gateway;
     private final LeaveRequestOutputBoundary outputBoundary;
@@ -16,6 +19,7 @@ public class LeaveRequestInteractor implements LeaveRequestInputBoundary {
 
     @Override
     public LeaveRequestResponseModel create(LeaveRequestRequestModel requestModel) {
+        // get current user and relevant data
         User user = Curr.getUser();
         int vacationDays = user.getVacationDays();
         int leaveDays = requestModel.getLeaveDays();
@@ -25,18 +29,31 @@ public class LeaveRequestInteractor implements LeaveRequestInputBoundary {
 
         if (!requestModel.isReturnAfter()) {
             return outputBoundary.prepareFailureView("Return date have to be after the start date.");
-        } else if (requestModel.getLeaveType() == LeaveType.VACATION & vacationDays < leaveDays) {
-            return outputBoundary.prepareFailureView("You do not have enough vacation days left\nRequested: " +
-                    leaveDays + " Remaining: " + vacationDays);
         }
 
-        user.setVacationDays(vacationDays - leaveDays);
+        if (requestModel.getLeaveType() == LeaveType.VACATION) {
+            if (vacationDays < leaveDays) {
+                return outputBoundary.prepareFailureView("You do not have enough vacation days left\nRequested: "
+                        + leaveDays + " Remaining: " + vacationDays);
+            }
+            user.setVacationDays(vacationDays - leaveDays); // update user's vacation days
+        }
+
+        // get all superiors of user
+        Set<Integer> members = new HashSet<>();
+        for (Project p : user.getProjects()) {
+            Integer head = p.getHead();
+            if (RoleAllowed.isHeadOf(gateway.getRoles(head), user.getRoles())) {
+                members.add(head);
+            }
+        }
 
         // e.g. "Tim Lan's VACATION Leave Request (2022-11-17 until 2022-12-12 return)"
         String projectName = user.getName() + "'s " + leaveType + " Leave Request (" + startDate + " until " + returnDate
                 + " return)";
-        Project project = projectBuilder.createProject(projectName, requestModel.getMessage());
+        Project project = projectBuilder.createProject(projectName, requestModel.getMessage(), members);
 
+        // save the project and all its tasks
         LeaveRequestDsRequestModel dsRequestModel = new LeaveRequestDsRequestModel(project);
         gateway.save(dsRequestModel);
 
