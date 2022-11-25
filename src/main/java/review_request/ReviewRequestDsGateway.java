75717e1d942b9156054ea9399eef47f07c6d1d63
tlan4 <tim.lan100102@gmail.com>
2022-11-25 17:43:44 -0500

    Modified Gateway and RequestModel.

--- src/main/java/review_request/ReviewRequestDsGateway.java
+++ src/main/java/review_request/ReviewRequestDsGateway.java
@@ -2,5 +2,4 @@ package review_request;
 
 public interface ReviewRequestDsGateway {
     void updateRequest(ReviewRequestDsRequestModel requestModel);
-    void setStatus(Integer uid, String status);
 }
