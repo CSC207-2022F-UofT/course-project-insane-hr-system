6917d472138e987194c1b4d84e2eca9d3e3cd3b2
KL.Wu <leonwu.wkl@gmail.com>
2022-12-06 00:19:27 -0500

    add CheckProfileMain to display the UI.

--- src/main/java/CheckProfileMain.java
+++ src/main/java/CheckProfileMain.java
@@ -29,14 +29,6 @@ public class CheckProfileMain {
 //    }
 
     public static void main(String[] args) {
-//        Table left = new Table(new String[]{"Employee Name"}, new Object[][]{new Object[]{"Bob"}, new Object[]{"john"}}, new Object[]{11,22});
-//        Table right = new Table(new String[]{"Head12 Name"}, new Object[][]{new Object[]{"Leon"}, new Object[]{"Alice"}}, new Object[]{11,22});
-//        String intro = String.format("Introduction:\nUid:\t%s\n", 1);
-//        IViewModel viewModel = new ViewModel(1, "Test Frame", "User Name", intro, left, right);
-//        viewModel.setVisualLevel(VisualLevel.ONLY_FACE);
-//        System.out.println(viewModel.toString());
-//        ScreenBuilder screenBuilder = new ScreenBuilder(viewModel);
-//        screenBuilder.show();
         CheckProfileIGateway gateway = new CheckProfileIMDataAccess();
         CheckProfileOutputBoundary presenter = new CheckProfilePresenter(new ViewModel());
         CheckProfileController controller = new CheckProfileController(presenter, gateway);
