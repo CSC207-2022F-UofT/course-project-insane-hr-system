e3cafe4215030a2b8b5299d1aa2b2bef8de9afa7
Zeyu Zhang <zeyuzh_zhang@hotmail.com>
2022-12-08 17:33:22 -0500

    add documentations for enroll employee use case

--- src/main/java/use_case/enroll_employee/EnrollInputBoundary.java
+++ src/main/java/use_case/enroll_employee/EnrollInputBoundary.java
@@ -3,7 +3,19 @@ package use_case.enroll_employee;
 import java.util.List;
 
 public interface EnrollInputBoundary{
+
+    /**
+     * Based on the user input information to create a new user.
+     * @param requestModel contains all the user input information
+     * @return a reponse model that contains the generated user id, username and user password.
+     */
     EnrollResponseModel create(EnrollRequestModel requestModel);
+
+
+    /**
+     * Get all the department names in our database
+     * @return a list string that contains the name of all the departments in our database.
+     */
     List<String> getAllDpts();
  
 }
