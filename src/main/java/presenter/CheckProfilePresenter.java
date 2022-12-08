7acd9dbafcf599346fbc0f09640ce6903dc5240e
KL.Wu <leonwu.wkl@gmail.com>
2022-12-08 00:23:44 -0500

    bug fixing

--- src/main/java/presenter/CheckProfilePresenter.java
+++ src/main/java/presenter/CheckProfilePresenter.java
@@ -145,7 +145,7 @@ public class CheckProfilePresenter implements CheckProfileOutputBoundary {
 
     @Override
     public void update() {
-        
+
     }
 
     public IViewModel getViewModel() {
